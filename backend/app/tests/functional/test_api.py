cda5ccac39d1ac5fb2dd9c33c3d7fbc34ea77334
friedrich <flsworld.since87@gmail.com>
2022-03-07 11:12:20 +0100

    add final functional tests

--- backend/app/tests/functional/test_api.py
+++ backend/app/tests/functional/test_api.py
@@ -1,9 +1,19 @@
+import urllib
+
+import pytest
+
 from app.core.config import settings
+from app.schemas.character import CHOICES as CHARACTER_CHOICES
 
 
 def test_create_comment(client):
     # Create character
-    data = {"name": "Foo", "status": "unknown", "species": "unknown", "gender": "unknown"}
+    data = {
+        "name": "Foo",
+        "status": "unknown",
+        "species": "unknown",
+        "gender": "unknown",
+    }
     response = client.post(f"{settings.API_PREFIX}/characters/", json=data)
     content = response.json()
     character_id = content["id"]
@@ -27,14 +37,62 @@ def test_create_comment(client):
     assert "id" in content
 
 
-def test_read_characters_under_pagination(setup):
-    pass
+def test_read_characters_under_pagination(client, setup):
+    response = client.get(f"{settings.API_PREFIX}/characters?skip=1&limit=2")
+    assert response.status_code == 200
+    content = response.json()
+    assert len(content) == 2
+    assert content[0]["id"] == 2
 
 
-def test_read_characters_with_filters():
-    pass
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ({"status": "Dead"}, 1),
+        ({"species": "unknown"}, 1),
+        ({"gender": "Female"}, 2),
+        ({"status": "Alive", "gender": "Male"}, 3),
+        ({"status": "Alive", "species": "Human", "gender": "Male"}, 2),
+    ],
+)
+def test_read_characters_with_filters(client, setup, test_input, expected):
+    params = urllib.parse.urlencode(test_input)
+    response = client.get(f"{settings.API_PREFIX}/characters?{params}")
+    assert response.status_code == 200
+    content = response.json()
+    assert len(content) == expected
 
 
-def test_read_episodes_with_search():
-    pass
+@pytest.mark.parametrize(
+    "field,value,values_set",
+    [
+        ("status", "dead", CHARACTER_CHOICES["status"]),
+        ("species", "alien", CHARACTER_CHOICES["species"]),
+        ("gender", "genderless", CHARACTER_CHOICES["gender"]),
+    ],
+)
+def test_read_characters_with_wrong_filters(client, field, value, values_set):
+    params = urllib.parse.urlencode({field: value})
+    response = client.get(f"{settings.API_PREFIX}/characters?{params}")
+    assert response.status_code == 422
+    content = response.json()
+    assert content["detail"] == [
+            {
+                "loc": ["query", field],
+                "msg": f"value must be in {values_set}",
+                "type": "value_error",
+            }
+        ]
 
+
+def test_read_episodes_with_search(client, setup):
+    data = {
+        "air_date__gte": "2022-03-01",
+        "air_date__lte": "2022-07-01",
+        "segment": None,
+        "name__icontains": "a",
+    }
+    response = client.post(f"{settings.API_PREFIX}/episodes/search", json=data)
+    assert response.status_code == 200
+    content = response.json()
+    assert len(content) == 4
